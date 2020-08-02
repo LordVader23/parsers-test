447fc35d56142887ff16bacf9aa9c29c248cf0e5
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-02 22:14:17 +0300

    Learning bs4

--- parser1_1.py
+++ parser1_1.py
@@ -1,7 +1,26 @@
 from bs4 import BeautifulSoup
 import re
 
+regexp = r'^\d{2}.\d{2}.\d{4}$'
+
 if __name__ == '__main__':
     html = open('index.html').read()
     soup = BeautifulSoup(html, 'lxml')
+
+    # div = soup.find('div', {'class': 'links'})
+    # print(div)
+
+    # links = soup.find_all('a')
+    # for a in links:
+    #     print(a.get('href'))
+
+    # div = soup.find('h1').find_parent('div', class_='two')
+    # print(div)
+
+    text = soup.find('h1').next_sibling
+    print(text)
+
+    div = soup.find('div', text=re.compile(regexp))
+    print(div.next)
+
 
