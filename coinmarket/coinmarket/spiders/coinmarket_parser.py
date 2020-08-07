52195c470b98f11ad80642ab1c29f242614c520c
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-07 22:15:51 +0300

    Changed extracting all data

--- coinmarket/coinmarket/spiders/coinmarket_parser.py
+++ coinmarket/coinmarket/spiders/coinmarket_parser.py
@@ -9,23 +9,27 @@ class CoinmarketParserSpider(scrapy.Spider):
     start_urls = ['http://https://coinmarketcap.com/all/views/all//']
 
     def parse(self, response):
-        names = response.css('div img + a.cmc-link::text').extract()
+        # names = response.css('div img + a.cmc-link::text').extract()
+        names = response.css('td a.cmc-link::text').extract()[0::3]
         symbols_dirty = response.css('td div::text').extract()
-        symbols = []
+        # symbols = []
+        symbols = response.css('td div::text').extract()[1::7]
 
-        for symbol in symbols_dirty:
-            if re.compile('^[A-Z]{3,5}$').findall(symbol.strip()):
-                symbols.append(symbol)
+        # for symbol in symbols_dirty:
+        #     if re.compile('^[A-Z]{3,5}$').findall(symbol.strip()):
+        #         symbols.append(symbol)
 
         price_links = response.css('td a.cmc-link::text').extract()
         price_hreves = response.css("td a.cmc-link::attr('href')").extract()
-        prices = []
-        links = []
+        prices = response.css('td a.cmc-link::text').extract()[1::3]
+        links = response.css("td a.cmc-link::attr('href')").extract()[1::3]
 
-        for (index, (link, href)) in enumerate(zip(price_links, price_hreves)):
-            if re.compile('^/currencies/').findall(href.strip()):
-                prices.append(link)
-                links.append('https://coinmarketcap.com/' + 'href')
+        # for (index, (link, href)) in enumerate(zip(price_links, price_hreves)):
+        #     if re.compile('^/currencies/').findall(href.strip()):
+        #         prices.append(link)
+        #         links.append('https://coinmarketcap.com/' + 'href')
+
+
 
 
 
