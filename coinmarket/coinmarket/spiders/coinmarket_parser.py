8c91631a8c1e9a631dfe0b38697a55d0230b9950
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-08 22:13:24 +0300

    Added few improvings

--- coinmarket/coinmarket/spiders/coinmarket_parser.py
+++ coinmarket/coinmarket/spiders/coinmarket_parser.py
@@ -9,25 +9,11 @@ class CoinmarketParserSpider(scrapy.Spider):
     start_urls = [r'https://coinmarketcap.com/all/views/all/']
 
     def parse(self, response):
-        # names = response.css('div img + a.cmc-link::text').extract()
         names = response.css('td a.cmc-link::text').extract()[0::3]
-        symbols_dirty = response.css('td div::text').extract()
-        # symbols = []
         symbols = response.css('td div::text').extract()[1::7]
-
-        # for symbol in symbols_dirty:
-        #     if re.compile('^[A-Z]{3,5}$').findall(symbol.strip()):
-        #         symbols.append(symbol)
-
-        price_links = response.css('td a.cmc-link::text').extract()
-        price_hreves = response.css("td a.cmc-link::attr('href')").extract()
         prices = response.css('td a.cmc-link::text').extract()[1::3]
         links = response.css("td a.cmc-link::attr('href')").extract()[1::3]
 
-        # for (index, (link, href)) in enumerate(zip(price_links, price_hreves)):
-        #     if re.compile('^/currencies/').findall(href.strip()):
-        #         prices.append(link)
-        #         links.append('https://coinmarketcap.com/' + 'href')
         row_data = zip(names, symbols, prices, links)
 
         for (name, symbol, price, link) in row_data:
@@ -35,7 +21,8 @@ class CoinmarketParserSpider(scrapy.Spider):
                 'page': response.url,
                 'name': name,
                 'price': price,
-                'link': link,
+                'symbol': symbol,
+                'link': r'https://coinmarketcap.com' + link,
             }
 
             yield scrapped_data
