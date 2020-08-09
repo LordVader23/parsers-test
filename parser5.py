21d459a25eeae1e300d6a4f60d289994537dd6bd
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-09 22:40:05 +0300

    Tested some code

--- parser5.py
+++ parser5.py
@@ -1,7 +1,17 @@
 import re
 
-if re.compile('^[A-Z]{3,5}$').findall('AB'.strip()):
-    print('yes')
+# if re.compile('^[A-Z]{3,5}$').findall('AB'.strip()):
+#     print('yes')
+#
+# for (index, (a, b)) in enumerate(zip(list(range(5)), list(range(5)))):
+#     print(index, a, b)
 
-for (index, (a, b)) in enumerate(zip(list(range(5)), list(range(5)))):
-    print(index, a, b)
+f = open(r'proxies.txt', 'r')
+
+for i in f:
+    # a = i.strip()
+    i = i.replace('\t', ' ')
+    i = i.replace('\n', '')
+    a = i.split(' ')
+    print(a)
+
