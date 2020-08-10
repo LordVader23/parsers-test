4d81947d02fbccfdcc95f785b4c5aa200e76f3fd
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-10 15:11:58 +0300

    Added custom settings for spider

--- coinmarket/coinmarket/spiders/proxy_test.py
+++ coinmarket/coinmarket/spiders/proxy_test.py
@@ -1,12 +1,21 @@
 from fake_useragent import UserAgent
 from random import choice
 import scrapy
+import datetime
 
 
 class ProxyTestSpider(scrapy.Spider):
     name = 'proxy_test'
     allowed_domains = ['coinmarketcap.com']
     start_urls = [r'https://coinmarketcap.com/']
+    custom_settings = {
+        'FEED_URI': 'proxy_test' + datetime.datetime.today().strftime('%y%m%d') + '.csv',
+        'FEED_FORMAT': 'csv',
+        # 'FEED_EXPORTERS': {
+        #     'json': 'scrapy.exporters.JsonItemExporter',
+        # },
+        'FEED_EXPORT_ENCODING': 'utf-8',
+    }
 
     def parse(self, response):
         yield response.follow(r'http://sitespy.ru/my-ip', callback=self.get_ip)
@@ -14,4 +23,6 @@ class ProxyTestSpider(scrapy.Spider):
     def get_ip(self, response):
         ip = response.css('span.ip::text').extract()
         user_agent = response.css('span.ip + br + span')
+        print(ip)
+        print(user_agent)
 
