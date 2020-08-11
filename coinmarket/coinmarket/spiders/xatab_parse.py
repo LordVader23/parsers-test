ce63a7485c609d4c957e7a54bf82976b1ba69191
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-11 16:20:50 +0300

    Added custom settings

--- coinmarket/coinmarket/spiders/xatab_parse.py
+++ coinmarket/coinmarket/spiders/xatab_parse.py
@@ -5,6 +5,14 @@ class XatabParseSpider(scrapy.Spider):
     name = 'xatab_parse'
     allowed_domains = ['v.otxataba.net']
     start_urls = ['https://v.otxataba.net/']
+    custom_settings = {
+        'FEED_URI': 'otxataba' + '.csv',
+        'FEED_FORMAT': 'csv',
+        # 'FEED_EXPORTERS': {
+        #     'json': 'scrapy.exporters.JsonItemExporter',
+        # },
+        'FEED_EXPORT_ENCODING': 'utf-8',
+    }
 
     def parse(self, response):
-        pass
+        
