033dc0a57c7c8fa5d2e6273e055b9ff180dc8beb
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-11 16:44:12 +0300

    Created func to parse links with pagination

--- coinmarket/coinmarket/spiders/xatab_parse.py
+++ coinmarket/coinmarket/spiders/xatab_parse.py
@@ -13,6 +13,20 @@ class XatabParseSpider(scrapy.Spider):
         # },
         'FEED_EXPORT_ENCODING': 'utf-8',
     }
+    page_num = 1
 
     def parse(self, response):
-        
+        links = response.css("div.entry__title h2 > a::attr('href')").extract()
+        num_of_pages = response.css("div.pagination span.nav_ext + a::attr('href')").extract()
+
+        yield from response.follow_all(links, self.parse_page)
+
+        if self.page_num < num_of_pages:
+            page_url = r'https://v.otxataba.net/page/{}/'.format(self.page_num)
+            XatabParseSpider.page_num += 1
+            yield response.follow(page_url, callback=self.parse)
+
+
+    def parse_page(self, response):
+        pass
+
