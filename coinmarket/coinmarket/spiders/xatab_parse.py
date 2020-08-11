2b8a80ac1424f201f6ad6eda2367a117ecbc72da
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-11 22:56:58 +0300

    Finished func to parse page

--- coinmarket/coinmarket/spiders/xatab_parse.py
+++ coinmarket/coinmarket/spiders/xatab_parse.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class XatabParseSpider(scrapy.Spider):
@@ -21,14 +22,33 @@ class XatabParseSpider(scrapy.Spider):
 
         yield from response.follow_all(links, self.parse_page)
 
-        if self.page_num < num_of_pages:
-            page_url = r'https://v.otxataba.net/page/{}/'.format(self.page_num)
-            XatabParseSpider.page_num += 1
-            yield response.follow(page_url, callback=self.parse)
+        # if self.page_num < num_of_pages:
+        #     page_url = r'https://v.otxataba.net/page/{}/'.format(self.page_num)
+        #     XatabParseSpider.page_num += 1
+        #     yield response.follow(page_url, callback=self.parse)
 
     def parse_page(self, response):
         title = response.css("div.inner-entry__allinfo h1.inner-entry__title::text").extract()
         year_of_issue = response.css("div.inner-entry__details::text").extract()[1]
-        genres = response.css("div.inner-entry__details a::text").extract()  # almost works
-        developer = response.css("div.inner-entry__details a + br + strong::text").extract()  # doesn't work
+        genres = response.css("div.inner-entry__details a::text").extract()
+
+        for (index, elem) in enumerate(genres):
+            if re.search(r'года$', elem.strip()):
+                del genres[index]
+            elif elem.strip() == 'Лицензии' or elem.strip() == 'Ожидаемые':
+                del genres[index]
+
+        some_string = response.css("div.inner-entry__details").extract()[0]  # Some shit to extract developer string(it works!)
+        developer = re.search(r'Разработчик: </strong> ([A-Z]{1}[a-z]+)<br>', some_string).group(1)
+        download_link = response.css(r"div[id='download'] a::attr('href')").extract()[0]
+
+        yield {
+            'page': response.url,
+            'title': title,
+            'year of issue': year_of_issue,
+            'genres': r'/'.join(genres),
+            'developer': developer,
+            'download link': download_link,
+        }
+
 
