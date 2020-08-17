06e85862a806ff4d468ba186242bde5d7ecdd395
LordVader23 <artem.fedotenko23@gmail.com>
2020-08-17 13:12:41 +0300

    Few changings

--- coinmarket/coinmarket/middlewares.py
+++ coinmarket/coinmarket/middlewares.py
@@ -120,10 +120,10 @@ class ProxyMiddleware(object):
     # overwrite process request
     def process_request(self, request, spider):
         # Set the location of the proxy
-        request.meta['proxy'] = "http://88.198.24.108:3128"
+        request.meta['proxy'] = "http://:@88.198.24.108:3128"
 
         # Use the following lines if your proxy requires authentication
-        proxy_user_pass = "USERNAME:PASSWORD"
+        proxy_user_pass = b"USERNAME:PASSWORD"
         # setup basic authentication for the proxy
-        encoded_user_pass = base64.encodestring(proxy_user_pass)
-        request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+        # encoded_user_pass = base64.encodebytes(proxy_user_pass).replace(b'\n', b'')
+        # request.headers['Proxy-Authorization'] = b'Basic ' + encoded_user_pass
